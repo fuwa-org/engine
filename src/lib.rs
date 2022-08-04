03ae042d438c3bd3b35c0fb517107ee969815e22
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-08-03 22:17:02 -0700

    Add AST and enum array

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,13 @@ use wasm_bindgen::{convert::WasmAbi, describe::WasmDescribe, prelude::*};
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[derive(Debug)]
+pub enum AstNode {
+    Program(Vec<AstNode>),
+    Number(f64),
+    String(String),
+    Comment(String),
+}
 
 #[derive(Parser)]
 #[grammar = "fuwa.pest"]
@@ -17,15 +24,25 @@ struct Parser;
 
 #[wasm_bindgen]
 pub fn parse(filename: String) {
-    let path = Path::new(&filename);
+    let mut ast = Vec::<AstNode>::new();
+
+    let path = Path::new(&filename); // converts string to path
     let contents = fs::read_to_string(path).unwrap();
     let pairs = Parser::parse(Rule::program, &contents).unwrap();
+    // TODO - add error handling
+    // TODO - add blocks
     pairs.for_each(|pair| match pair.as_rule() {
-        Rule::program => {
-            println!("{:?}", pair);
+        Rule::COMMENT => {
+            let comment = pair.as_str().to_string();
+            ast.push(AstNode::Comment(comment));
+        }
+        Rule::STRING => {
+            let statement = pair.as_str().to_string();
+            ast.push(AstNode::String(statement));
         }
         _ => {
-            println!("{:?}", pair);
+            println!("Other {}", pair.as_str());
         }
     });
+    println!("{:?}", ast);
 }
